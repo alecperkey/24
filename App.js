f3f4fcb01806a73a590a4fd4672a3a0cf5b72af5
Alec <alecperkey@gmail.com>
2018-04-10 14:56:14 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import Navigator from "./Navigator";
+import { getToken } from "./loginUtils";
+
+const authLink = setContext(async (req, { headers }) => {
+  const token = await getToken();
+  console.log(token);
+  return {
+    ...headers,
+    headers: {
+      authorization: token ? `Bearer ${token}` : null
+    }
+  };
+});
+
+const httpLink = new HttpLink({
+  uri: "https://api.graph.cool/simple/v1/cjckpsddm477j0187nf9v89oi"
+});
+
+const link = authLink.concat(httpLink);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Navigator />
+      </ApolloProvider>
+    );
+  }
+}
